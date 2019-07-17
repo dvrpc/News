10a3ea1b6671908d48aaf08abd1f5d44542bce05
Marc Molta <mcmolta10@gmail.com>
2019-07-17 15:33:22 -0400

    set up archive layout/functionality & prep for api response

--- Archive/js/index.js
+++ Archive/js/index.js
@@ -9,8 +9,90 @@ const getPosts = async () => {
     }
 }
 
-const posts = getPosts()
+const response = getPosts()
 
-posts.then(post => {
-    console.log('post ', post)
+response.then(posts => {
+    console.log('posts ', posts)
+    if(posts) {
+        const list = document.querySelector('#archive-ul')
+        //posts.forEach
+    }else{
+        console.log('error ')
+    }
 })
+
+// build da ting
+/*
+    General Layout:
+        <ul id="details-wrapper">    
+            <details class="year-details">
+                <summary class="year-summary">The Year</summary>
+                <details class="month-details">
+                    <summary class="month-summary">The Month</summary>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <h2>Post Title</h2>
+                            <img class="post-img" src="base64 img string" alt="post title + img" />
+                            <p>Post Content</p>
+                            <a href="Post Link" rel="external">Read More</a>
+                        </li>
+                    </ul>
+                </details>
+            </details>
+        </ul>
+
+    Parse the date created for each response item and make the following decisions:
+        **IF the year and month exist, create the following template string:
+            const entry = `
+                <li class="list-group-item">
+                    <h2>{Post Title}</h2>
+                    <img class="post-img" src="base64 img string" alt={post title} image" />
+                    <p>{Post Content}</p>
+                    <a href="{Post Link}" rel="external">Read More</a>
+                </li>
+            `
+            get a handle on the correct month's <ul>
+            <ul>.insertAdjacentHTML('beforeend', entry)
+
+        **IF the year exists but the month doesn't, create the following template string:
+            const entryWithMonth = `
+                <details class="month-details">
+                    <summary class="month-summary">{The Month}</summary>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <h2>{Post Title}</h2>
+                            <img class="post-img" src="base64 img string" alt={post title} image" />
+                            <p>{Post Content}</p>
+                            <a href="{Post Link}" rel="external">Read More</a>
+                        </li>
+                    </ul>
+                </details>
+            `
+            get a handle on the correct year's <details>
+            <details>.insertAdjacentHTML('beforeend', entryWithMonth)
+
+        **IF neither the year nor the month exist, create the following template string:
+            const entryWithMonthAndYear = `
+                <details class="year-details">
+                    <summary>{The Year}</summary>
+                    <details class="month-details">
+                        <summary class="month-summary">{The Month}</summary>
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <h2>{Post Title}</h2>
+                                <img class="post-img" src="base64 img string" alt={post title} image" />
+                                <p>{Post Content}</p>
+                                <a href="{Post Link}" rel="external">Read More</a>
+                            </li>
+                        </ul>
+                    </details>
+                </details>
+            `
+            get a handle on <ul id="details-wrapper">
+            <ul>.insertAdjacentHTML('beforeend', entryWithMonthAndYear)
+
+    Lazy load each block of posts. Check if they exist first before making the db call to avoid redundant calls
+*/
+const buildListItems = post => {
+    
+}
